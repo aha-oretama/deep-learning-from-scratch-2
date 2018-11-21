bc7d27702417ab813403960149ad3bf2edb75bfe
aha-oretama <sekine_y_529@msn.com>
2018-11-21 22:36:50 +0900

    Ch3.4.0

---
+++ chapter3/simple_cbow.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+# coding: utf-8
+import sys
+import numpy as np
+
+sys.path.append('..')
+from common.layers import MatMul, SoftmaxWithLoss
+
+
+class SimpleCBOW:
+
+    def __init__(self, vocab_size, hidden_size):
+        V, H = vocab_size, hidden_size
+
+        # 重みの初期化
+        W_in = 0.01 * np.random.randn(V, H).astype('f')
+        W_out = 0.01 * np.random.randn(H,V).astype('f')
+
+        # レイヤの生成
+        self.in_layer0 = MatMul(W_in)
+        self.in_layer1 = MatMul(W_in)
+        self.out_layer = MatMul(W_out)
+        self.loss_layer = SoftmaxWithLoss()
+
+        # すべての重みと勾配をリストにまとめる
+        layers = [self.in_layer0, self.in_layer1, self.out_layer]
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        self.word_vecs = W_in
+
+    def forward(self, contexts, target):
+        h0 = self.in_layer0.forward(contexts[:, 0])
+        h1 = self.in_layer1.forward(contexts[:, 1])
+        h = 0.5 * (h0 + h1)
+        score = self.out_layer.forward(h)
+        loss = self.loss_layer.forward(score, target)
+        return loss
+
+    def backward(self, dout = 1):
+        ds = self.loss_layer.backward(dout)
+        da = self.out_layer.backward(ds)
+        da *= 0.5
+        self.in_layer0.backward(da)
+        self.in_layer1.backward(da)
+        return None
