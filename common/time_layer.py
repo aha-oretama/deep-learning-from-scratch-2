d0a84e6bdfd3c7aa9caa9f66a592546ebe325e1c
aha-oretama <sekine_y_529@msn.com>
2018-12-09 11:10:04 +0900

    Ch6.3.0 forward

--- common/time_layer.py
+++ common/time_layer.py
@@ -5,7 +5,7 @@ import numpy as np
 sys.path.append('..')
 
 from common.layers import Embedding
-from common.functions import softmax
+from common.functions import softmax, sigmoid
 
 
 class RNN:
@@ -212,3 +212,40 @@ class TimeSoftmaxWithLoss:
         dx = dx.reshape((N, T, V))
 
         return dx
+
+
+class LSTM:
+    def __init__(self, Wx, Wh, b) -> None:
+        '''
+        Parameters
+        ----------
+        Wx: 入力`x`用の重みパラーメタ（4つ分の重みをまとめる）
+        Wh: 隠れ状態`h`用の重みパラメータ（4つ分の重みをまとめる）
+        b: バイアス（4つ分のバイアスをまとめる）
+        '''
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.cache = None
+
+    def forward(self, x, h_prev, c_prev):
+        Wx, Wh, b = self.params
+        N, H = h_prev.shape
+
+        A = np.dot(x, Wx) + np.dot(h_prev, Wh) + b
+
+        # slice
+        f = A[:, :H]
+        g = A[:, H:2 * H]
+        i = A[:, 2 * H:3 * H]
+        o = A[:, 3 * H:]
+
+        f = sigmoid(f)
+        g = np.tanh(g)
+        i = sigmoid(i)
+        o = sigmoid(o)
+
+        c_next = c_prev * f + g * i
+        h_next = o * np.tanh(c_next)
+
+        self.cache = (x, h_prev, c_prev, i, f, g, o, c_next)
+        return h_next, c_next
